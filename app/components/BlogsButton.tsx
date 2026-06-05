2953bbcb16e02ab7486c5202d5e159e21ed5bf15
Shayan Ahmed <shayanahmed675432@gmail.com>
2026-06-05 14:36:08 +0500

    remove blog button from admin

--- app/components/BlogsButton.tsx
+++ app/components/BlogsButton.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function BlogsButton() {
+  const pathname = usePathname();
+
+  if (pathname?.startsWith('/admin')) {
+    return null;
+  }
+
   return (
     <Link
       href="/blogs"
